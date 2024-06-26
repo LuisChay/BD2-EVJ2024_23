39fb770d267435775794882329170d61fb13d270
Luis <luisinmanuel123@gmail.com>
2024-06-26 00:03:17 -0600

    [ADD] Catalogo y Registro libros

--- Proyecto2/frontend/src/App.js
+++ Proyecto2/frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import AdminLibros from './pages/adminLibros';
+import Catalogo from './pages/catalogo';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/administrador/libros" element={<AdminLibros/>}/>
+        <Route path="/catalogo" element={<Catalogo/>}/>
+      </Routes>
+    </Router>
+
+    </>
   );
 }
 
