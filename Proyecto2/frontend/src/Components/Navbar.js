df64b165e734f769c93656d4994ea3e68300715e
Katherinne Gómez <3010587170101@ingenieria.usac.edu.gt>
2024-06-26 23:50:41 -0600

    Cambios Navbar

---
+++ Proyecto2/frontend/src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import {Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar(){
+    return(
+    <>
+        <nav className="nav">
+          <Link to="/" className="site-title">
+            Proyecto2 BD2
+          </Link>
+          <ul>
+            <CustomLink to="/Historial">Historial</CustomLink>
+          </ul>
+        </nav>
+    </>
+    )
+    
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    )
+}
